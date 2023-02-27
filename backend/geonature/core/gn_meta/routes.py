0ff1d41025647ba2037bb8dbfd5eccb8e414c8c7
jbrieuclp <jbrieuclp@gmail.com>
2023-02-27 15:12:02 +0100

    Add diffusion-level form in datasets elements

--- backend/geonature/core/gn_meta/routes.py
+++ backend/geonature/core/gn_meta/routes.py
@@ -178,6 +178,7 @@ def get_dataset(scope, id_dataset):
             "nomenclature_data_origin",
             "nomenclature_source_status",
             "nomenclature_resource_type",
+            "nomenclature_diffusion_level",
             "cor_territories",
             "acquisition_framework",
             "acquisition_framework.creator",
