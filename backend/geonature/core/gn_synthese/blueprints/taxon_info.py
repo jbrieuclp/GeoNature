90050ffcf0236e6a71456b4b9ce19920d0de76a8
jbrieuclp <jbrieuclp@gmail.com>
2025-08-06 16:23:06 +0200

    amelioration de la recherche taxo par synonyme ou nom valide (si non present en synthese)

--- backend/geonature/core/gn_synthese/blueprints/taxon_info.py
+++ backend/geonature/core/gn_synthese/blueprints/taxon_info.py
@@ -25,7 +25,7 @@ from utils_flask_sqla.generic import GenericTable
 from utils_flask_sqla.response import json_resp
 
 
-from sqlalchemy import desc, distinct, func, select, join, exists
+from sqlalchemy import desc, distinct, func, select, join, exists, or_
 from sqlalchemy.orm import Query
 from werkzeug.exceptions import BadRequest, Forbidden
 
@@ -147,7 +147,8 @@ def get_autocomplete_taxons_synthese():
             ),
         )
         .distinct()
-        .join(Synthese, Synthese.cd_nom == VMTaxrefListForautocomplete.cd_nom)
+        .join(Taxref, or_(Taxref.cd_nom == VMTaxrefListForautocomplete.cd_nom, Taxref.cd_ref == VMTaxrefListForautocomplete.cd_nom))
+        .join(Synthese, Synthese.cd_nom == Taxref.cd_nom)
     )
     search_name = search_name.replace(" ", "%")
     query = query.where(
